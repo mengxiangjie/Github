a14afb4734d0bea3a77e952b1cffaee6e1a8e4f2
mengxiangjie <jeremysmg0530@gmail.com>
2017-01-26 15:41:50 -0500

    week 2 notes

--- Week 2/Week 2 Notes/sketch.js
+++ Week 2/Week 2 Notes/sketch.js
@@ -1,7 +1,29 @@
-function setup() {
+var num = 100;
+var num2 = 50;
+var adidas = 1000;
+var xiangjie = "I";
+var xiangjie2 = "AM";
+var xiangjie3 = "GOD";
+var message = "hello";
+var anotherMessage = "byebye";
+var booleanVar = true;
+var booleanVar2 = false;
 
+function setup() {
+console.log(num);
+console.log("num + num2=" +(num + num2));
+console.log(num * num2);
+console.log(num/num2);
+console.log(message + anotherMessage);
+console.log(message + " " + anotherMessage);
+console.log(xiangjie + " "+ xiangjie2 + " "+ xiangjie3);
+console.log(booleanVar);
+
+console.log(message + adidas);
+
+    
 }
 
 function draw() {
-  
+  console.log(num)
 }
