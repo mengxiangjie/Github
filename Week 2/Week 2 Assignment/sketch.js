8f581ffdbab2ca32c651ce625f06987f1f111add
mengxiangjie <jeremysmg0530@gmail.com>
2017-02-09 01:00:04 -0500

    Week 3 assignment

--- Week 2/Week 2 Assignment/sketch.js
+++ Week 2/Week 2 Assignment/sketch.js
@@ -2,34 +2,54 @@ var mouthX = 800;
 var mouthY = 800;
 var mouthW = 400;
 var mouthH = 60;
-var CanvasW = 800;
-var CanvasH = 800;
-var canvasColor = "yellow"
-var circleX = 200;
-var circleY = 200;
-var fillColor = "black"
-var rectwidth = 20;
-var rectheight = 20;
+var CanvasW = 500;
+var CanvasH = 500;
+var canvasColor = "black";
+var squarewidth = 200;
+var squareheight = 200;
+var fillColor = "none filled";
+var trianglewidth = 250;
+var triangleheight = 110;
+
 
 
 function setup() {
-    createCanvas(800,800);
-    background (255,211,100);
-    
-   
+    createCanvas(500,500);
+    background(0,0,0);
+
     console.log("Canvas Width:" + CanvasW + "      " + "Canvas Height:" + CanvasH);
     console.log("Canvas Color:" + canvasColor);
-    console.log("circle X:" + circleX + "      " + "circle Y:" + circleY);
     console.log("Fill Color:" + fillColor);
-    console.log("Rectangle Area:" + rectwidth*rectheight);
+    console.log("Square Area:" + squarewidth*squareheight);
+    console.log("Triangle Area:" + (trianglewidth*triangleheight)/2);
     
+
 }
 
 function draw() {
-    fill(0,0,0)
-    ellipse(200,200,150,150);
-    line(10,10,100,mouthY/2);
-    rect(50,50,20,20);
+    
+    //House body
+    fill(195,188,181);
+    rect(150,220,200,200);
+    
+    
+    //Roof
+    fill(62,114,186);
+    triangle(250,110,375,220,125,220);
+    
+    
+    //Chimney
+    fill(84,29,25);
+    rect(290,70,30,110);
+    
+    
+    //Window
+    fill(0,0,0);
+    rect(260,280,50,50);
+  
     
   
 }
+
+   
+    
--- Week 3/Assignment/sketch.js
+++ Week 3/Assignment/sketch.js
@@ -1,7 +1,59 @@
+var backgroundColor = 'black';
+var col = 0;
+var value = 0;
+
+
 function setup() {
+    
+    createCanvas(500,500);
+    background(0,0,0);
 
 }
 
 function draw() {
+
+    //background
+    col = mouseX/2;
+    background(col);
+    
+    
+    
+    //House body
+    fill(195,188,181);
+    rect(150,220,200,200);
+    
+    
+    //Roof
+    fill(62,114,186);
+    triangle(250,110,375,220,125,220);
+    
+    
+    //Chimney
+    fill(84,29,25);
+    rect(290,70,30,110);
+    
+    
+    //Window
+    fill(0,0,0);
+    rect(260,280,50,50);
+    
+    //light
+    fill(value);
+    arc(285, 280, 30, 40, 0, PI, OPEN);
+
   
+    
+    
+}
+
+function mousePressed(){
+    
+value = 'lightyellow';
+   
+   
+}
+
+function mouseReleased(){
+    value = 0;
+    
 }
